b750111e4ae97805074552fd256b9b1b57955f53
minyeongChoi <42730692+minyeongChoi@users.noreply.github.com>
2021-06-29 02:57:55 +0900

    Initial commit

---
+++ src/main/java/com/example/springtt/SpringttApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringttApplication.class, args);
+    }
+
+}
